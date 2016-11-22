066b967f084848d1e9b87a27b47d4006686694d3
margarh1 <ha.margaret1@gmail.com>
2016-11-21 18:26:27 -0800

    added mongodb uri, ready to test deploy attempt 2

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/bucketlist');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bucketlist');
 
 module.exports.User = require('./user.js');
 module.exports.Wish = require('./wish.js');
